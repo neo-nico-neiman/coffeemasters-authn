94bfeefc8e75a54ea8a0e49136e1e9d73693d005
Nico Neiman <niconeiman@gmail.com>
2023-11-13 14:06:45 -0600

    simple login

--- public/scripts/Auth.js
+++ public/scripts/Auth.js
@@ -2,39 +2,104 @@ import API from "./API.js";
 import Router from "./Router.js";
 
 const Auth = {
-    isLoggedIn: false,
-    account: null,
-    updateStatus() {
-        if (Auth.isLoggedIn && Auth.account) {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "none"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".account_name").forEach(
-                e => e.innerHTML = Auth.account.name
-            );
-            document.querySelectorAll(".account_username").forEach(
-                e => e.innerHTML = Auth.account.email
-            );
+	isLoggedIn: false,
+	account: null,
+	postLogin: async (response, credentials) => {
+		if (response.ok) {
+			Auth.isLoggedIn = true;
+			Auth.account = { ...response.user };
+			Auth.updateStatus();
+			Router.go("/account");
+		} else {
+			alert(response.message);
+		}
 
-        } else {
-            document.querySelectorAll(".logged_out").forEach(
-                e => e.style.display = "block"
-            );
-            document.querySelectorAll(".logged_in").forEach(
-                e => e.style.display = "none"
-            );
+		// Credential management API storage to ahcieve auto login
+		if (window.PasswordCredential && credentials.password) {
+			const credentialsToStore = new PasswordCredential({
+				id: credentials.email,
+				password: credentials.password,
+				name: credentials.email,
+			});
 
-        }
-    },    
-    init: () => {
-        
-    },
-}
+			try {
+				navigator.credentials.store(credentialsToStore);
+			} catch {
+				console.log("Error"); // TODO: improve
+			}
+		}
+	},
+	autoLogin: async () => {
+		if (window.PasswordCredential) {
+			const credentials = await navigator.credentials.get({ password: true });
+			document.getElementById("login_email").value = credentials.id;
+			document.getElementById("login_password").value = credentials.password;
+		}
+	},
+	login: async (event) => {
+		event.preventDefault();
+
+		const credentials = {
+			email: document.getElementById("login_email").value,
+			password: document.getElementById("login_password").value,
+		};
+
+		if (credentials.email && credentials.password) {
+			const response = await API.login(credentials);
+			Auth.postLogin(response, credentials);
+		}
+	},
+	register: async (event) => {
+		event.preventDefault();
+		const user = {
+			name: document.getElementById("register_name").value,
+			email: document.getElementById("register_email").value,
+			password: document.getElementById("register_password").value,
+		};
+
+		if (user.name && user.email && user.password) {
+			const response = await API.register(user);
+			if (response.ok) {
+			}
+		}
+	},
+	logout: () => {
+		Auth.account = null;
+		Auth.isLoggedIn = false;
+		Auth.updateStatus();
+		Router.go("/");
+
+		if (window.PasswordCredential) {
+			navigator.credentials.preventSilentAccess();
+		}
+	},
+	updateStatus() {
+		if (Auth.isLoggedIn && Auth.account) {
+			document
+				.querySelectorAll(".logged_out")
+				.forEach((e) => (e.style.display = "none"));
+			document
+				.querySelectorAll(".logged_in")
+				.forEach((e) => (e.style.display = "block"));
+			document
+				.querySelectorAll(".account_name")
+				.forEach((e) => (e.innerHTML = Auth.account.name));
+			document
+				.querySelectorAll(".account_username")
+				.forEach((e) => (e.innerHTML = Auth.account.email));
+		} else {
+			document
+				.querySelectorAll(".logged_out")
+				.forEach((e) => (e.style.display = "block"));
+			document
+				.querySelectorAll(".logged_in")
+				.forEach((e) => (e.style.display = "none"));
+		}
+	},
+	init: () => {},
+};
 Auth.updateStatus();
-
+Auth.autoLogin();
 export default Auth;
 
 // make it a global object
--- server.js
+++ server.js
@@ -1,42 +1,85 @@
-import express from 'express'
-import { Low } from 'lowdb'
-import { JSONFile } from 'lowdb/node'
-import * as url from 'url';
-import bcrypt from 'bcryptjs';
-import * as jwtJsDecode from 'jwt-js-decode';
+import express from "express";
+import { Low } from "lowdb";
+import { JSONFile } from "lowdb/node";
+import * as url from "url";
+import bcrypt from "bcryptjs";
+import * as jwtJsDecode from "jwt-js-decode";
 import base64url from "base64url";
-import SimpleWebAuthnServer from '@simplewebauthn/server';
+import SimpleWebAuthnServer from "@simplewebauthn/server";
 
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-const adapter = new JSONFile(__dirname + '/auth.json');
+const adapter = new JSONFile(__dirname + "/auth.json");
 const db = new Low(adapter);
 await db.read();
-db.data ||= { users: [] }
+db.data ||= { users: [] };
 
 const rpID = "localhost";
 const protocol = "http";
 const port = 5050;
 const expectedOrigin = `${protocol}://${rpID}:${port}`;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
-app.use(express.urlencoded({
-  extended: true
-}));
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
 
-// ADD HERE THE REST OF THE ENDPOINTS
+function findUser(email) {
+	const results = db.data?.users.filter((user) => user.email == email);
+	if (results.length == 0) return undefined;
+	return results[0];
+}
 
+app.post("/auth/login", (req, res) => {
+	const { email, password } = req.body;
 
+	const user = findUser(email);
+	if (!user) {
+		res.status(401).send({ ok: false, message: "Credentials are wrong" });
+	}
+
+	if (!bcrypt.compareSync(password, user.password)) {
+		res.status(401).send({ ok: false, message: "Credentials are wrong" });
+	}
+
+	res
+		.status(200)
+		.send({ ok: true, user: { name: user.name, email: user.email } });
+});
+
+app.post("/auth/register", (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (findUser(email)) {
+		res.status(501).send({ ok: false, message: "User already exists" });
+	} else {
+		//TODO: add validation
+		const salt = bcrypt.genSaltSync(10);
+		const hashedPassword = bcrypt.hashSync(password, salt);
+
+		const user = {
+			name: name,
+			email: email,
+			password: hashedPassword,
+		};
+
+		db.data.users.push(user);
+		db.write();
+
+		res.status(200).send({ ok: true, message: "User registered" });
+	}
+});
 
 app.get("*", (req, res) => {
-    res.sendFile(__dirname + "public/index.html"); 
+	res.sendFile(__dirname + "public/index.html");
 });
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+	console.log(`App listening on port ${port}`);
 });
-
